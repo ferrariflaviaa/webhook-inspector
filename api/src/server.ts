385b5f84d6b22143c1dff27fce59dd782095f1a6
Flávia Ferrari <flaviaferrarit@gmail.com>
2025-11-22 16:08:36 -0300

    feat: create api structure with docker, drizzle and fastify

---
+++ api/src/server.ts
@@ -0,0 +1,54 @@
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from "./routes/list-webhooks";
+// import { listWebhooks } from './routes/list-webhooks'
+import { env } from './env'
+// import { getWebhook } from './routes/get-webhook'
+// import { deleteWebhook } from './routes/delete-webhook'
+// import { captureWebhook } from './routes/capture-webhook'
+// import { generateHandler } from './routes/generate-handler'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
+// app.register(getWebhook)
+// app.register(deleteWebhook)
+// app.register(captureWebhook)
+// app.register(generateHandler)
+
+app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
+	console.log("🔥 HTTP server running on http://localhost:3333");
+	console.log("📚 Docs available at http://localhost:3333/docs");
+});
