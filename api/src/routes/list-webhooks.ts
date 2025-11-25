e86b797b5981f367a374c2ba6700f60d2a1dc23d
Flávia Ferrari <flaviaferrarit@gmail.com>
2025-11-24 22:34:58 -0300

    feat: api routes and front-end integration

--- api/src/routes/list-webhooks.ts
+++ api/src/routes/list-webhooks.ts
@@ -1,8 +1,8 @@
-// import { db } from '@/db'
-// import { webhooks } from '@/db/schema'
-// import { desc } from 'drizzle-orm'
-// import { lt } from 'drizzle-orm'
-// import { createSelectSchema } from 'drizzle-zod'
+import { db } from '@/db'
+import { webhooks } from '@/db/schema'
+import { desc } from 'drizzle-orm'
+import { lt } from 'drizzle-orm'
+import { createSelectSchema } from 'drizzle-zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -18,45 +18,43 @@ export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
           cursor: z.string().optional(),
         }),
         response: {
-          200: z.array(  z.object({id: z.string(),
-            // webhooks: z.array(
-            //   createSelectSchema(webhooks).pick({
-            //     id: true,
-            //     method: true,
-            //     pathname: true,
-            //     createdAt: true,
-            //   })
-            // ),
-            // nextCursor: z.string().nullable(),
-          }))
+          200: z.object({
+            webhooks: z.array(
+              createSelectSchema(webhooks).pick({
+                id: true,
+                method: true,
+                pathname: true,
+                createdAt: true,
+              })
+            ),
+            nextCursor: z.string().nullable(),
+          })
         },
       },
     },
     async (request, reply) => {
       const { limit, cursor } = request.query
 
-      // const result = await db
-      //   .select({
-      //     id: webhooks.id,
-      //     method: webhooks.method,
-      //     pathname: webhooks.pathname,
-      //     createdAt: webhooks.createdAt,
-      //   })
-      //   .from(webhooks)
-      //   .where(cursor ? lt(webhooks.id, cursor) : undefined)
-      //   .orderBy(desc(webhooks.id))
-      //   .limit(limit + 1)
+      const result = await db
+        .select({
+          id: webhooks.id,
+          method: webhooks.method,
+          pathname: webhooks.pathname,
+          createdAt: webhooks.createdAt,
+        })
+        .from(webhooks)
+        .where(cursor ? lt(webhooks.id, cursor) : undefined)
+        .orderBy(desc(webhooks.id))
+        .limit(limit + 1)
 
-      // const hasMore = result.length > limit
-      // const items = hasMore ? result.slice(0, limit) : result
-      // const nextCursor = hasMore ? items[items.length - 1].id : null
+      const hasMore = result.length > limit
+      const items = hasMore ? result.slice(0, limit) : result
+      const nextCursor = hasMore ? items[items.length - 1].id : null
 
-      // return reply.send({
-      //   webhooks: items,
-      //   nextCursor,
-      // })
-
-      return [{id: '1234'}]
+      return reply.send({
+        webhooks: items,
+        nextCursor,
+      })
     },
   )
 }
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-import { fastify } from "fastify";
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
@@ -8,13 +8,11 @@ import {
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifyCors } from '@fastify/cors'
 import ScalarApiReference from '@scalar/fastify-api-reference'
-import { listWebhooks } from "./routes/list-webhooks";
-// import { listWebhooks } from './routes/list-webhooks'
+import { listWebhooks } from './routes/list-webhooks'
 import { env } from './env'
-// import { getWebhook } from './routes/get-webhook'
-// import { deleteWebhook } from './routes/delete-webhook'
-// import { captureWebhook } from './routes/capture-webhook'
-// import { generateHandler } from './routes/generate-handler'
+import { getWebhook } from './routes/get-webhook'
+import { deleteWebhook } from './routes/delete-webhook'
+import { captureWebhook } from './routes/capture-webhook'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -43,12 +41,11 @@ app.register(ScalarApiReference, {
 })
 
 app.register(listWebhooks)
-// app.register(getWebhook)
-// app.register(deleteWebhook)
-// app.register(captureWebhook)
-// app.register(generateHandler)
+app.register(getWebhook)
+app.register(deleteWebhook)
+app.register(captureWebhook)
 
-app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
-	console.log("🔥 HTTP server running on http://localhost:3333");
-	console.log("📚 Docs available at http://localhost:3333/docs");
-});
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('🔥 HTTP server running on http://localhost:3333!')
+  console.log('📚 Docs available at http://localhost:3333/docs')
+})
